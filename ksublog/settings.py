388480d38535b1353b92b684813908932ec9cbf4
QuatreMains <plutokor@gmail.com>
2019-03-12 21:41:23 +0900

    update fork version

--- ksublog/settings.py
+++ ksublog/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm9ses0r50r=_$3f=*bi86_%1s2k2kzpj1)(6hzn7%lc6$7pu*7'
+# SECRET_KEY = 'm9ses0r50r=_$3f=*bi86_%1s2k2kzpj1)(6hzn7%lc6$7pu*7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'm9ses0r50r=_$3f=*bi86_%1s2k2kzpj1)(6hzn7%lc6$7pu*7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
